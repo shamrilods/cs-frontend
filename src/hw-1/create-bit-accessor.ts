87d3a54911254d26387abe9ae1f9240ffe250391
Dmitry Shamrilo <shamrilods@gmail.com>
2023-04-14 12:07:08 +0300

    adds hw-1

---
+++ src/hw-1/create-bit-accessor.ts
@@ -0,0 +1,40 @@
+import { Bit, TypedArray } from './types'
+
+export class CreateBitAccessor {
+  private array: TypedArray
+
+  constructor(array: TypedArray) {
+    this.array = array
+  }
+
+  private validate(arrayIndex: number, bitIndex: number) {
+    if (arrayIndex < 0) {
+      throw Error('Array index < 0')
+    }
+    if (this.array.length <= arrayIndex) {
+      throw Error('Array index > array length')
+    }
+
+    if (bitIndex < 0) {
+      throw Error('Bit index < 0')
+    }
+
+    if (this.array.BYTES_PER_ELEMENT * 8 <= bitIndex) {
+      throw Error('Bit index < 0')
+    }
+  }
+
+  public get(arrayIndex: number, bitIndex: number): Bit {
+    this.validate(arrayIndex, bitIndex)
+
+    return (this.array[arrayIndex] & (1 << bitIndex)) === 0 ? 0 : 1
+  }
+
+  public set(arrayIndex: number, bitIndex: number, value: Bit) {
+    this.validate(arrayIndex, bitIndex)
+
+    this.array[arrayIndex] = value
+      ? this.array[arrayIndex] | (1 << bitIndex)
+      : this.array[arrayIndex] & ~(1 << bitIndex)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('init')
+import { CreateBitAccessor } from './hw-1'
+
+const bitAccessor = new CreateBitAccessor(new Uint8Array([0b1110, 0b1101]))
+
+console.log(bitAccessor.get(0, 1))
+bitAccessor.set(0, 1, 0)
+console.log(bitAccessor.get(0, 1))
